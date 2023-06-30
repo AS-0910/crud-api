ed666b518fe39cebad342510e4d2891f93be61db
Ankur Singh <88249359+AS-0910@users.noreply.github.com>
2023-06-30 14:23:53 +0530

    auto increment in sitenum added

--- site/controller.js
+++ site/controller.js
@@ -3,7 +3,6 @@ const client=connectDB();
 client.connect();
 const queries=require('./queries');
 
-
 const getSite=(req,res)=>{
     client.query(queries.getSites,(error,result)=>{
         if(error){
@@ -24,14 +23,46 @@ const getSiteById=(req,res)=>{
     });
 }
 
+let cnt=100;
 const addSite=(req,res)=>{
+    cnt=cnt+1;
+
     const site={
-        num:req.body.siteNum,
+        // num:req.body.siteNum,
         siteName:req.body.siteName,
         createdBy:req.body.createdBy
     }
+
+    let no=cnt;
+    // console.log(no);
+   
+    
+    // while(no>0){
+    //    console.log(no);
+    //    let nno=no/10;
+    //    no=nno;
+    //    sz=sz+1; 
+    // }
+
+    let numString = Math.abs(no).toString();
+    sz=numString.length;
+    // console.log(sz);
+    
+    id="TMS";
+    req=5-sz;
+    // console.log(req);
+    for(let i=1;i<=req;i++){
+       id=id.concat("0");
+    //    console.log(id);
+    }
+
     // console.log(site.siteName);
-    client.query(queries.addSites,[site.num,site.siteName,site.createdBy],(error,result)=>{
+    // id="TMS";
+    // n=toString(cnt);
+    // console.log('TMS'+n);
+    id=id.concat(cnt.toString());
+    console.log(id);
+    client.query(queries.addSites,[id,site.siteName,site.createdBy],(error,result)=>{
         if(error){
             throw error;
         }
