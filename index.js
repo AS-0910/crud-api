5da7caf4dfc2f304279bacaa0f9efde11082add0
Ankur Singh <88249359+AS-0910@users.noreply.github.com>
2023-07-07 11:35:15 +0530

    pushed site and step master api

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const siteRoutes=require('./app/routes/site.routes.js');
 const stepRoutes=require('./app/routes/step_master.routes.js');
 
 const app=express();
-const port=5000;
+const port=6000;
 
 
 app.use(express.json());
